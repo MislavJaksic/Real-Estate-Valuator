1000c91416c2fec759ff00cb7def70a4551aaeb0
MislavJaksic <jaksicmislav@gmail.com>
2017-08-13 11:51:08 +0200

    InputControl README
    InputControl README has been updated.

--- InputControl/README.md
+++ InputControl/README.md
@@ -1,3 +1,10 @@
 ## Input Control
 
 Input Control is responsible for verifying all inputs.
+
+### InputController
+
+Responsibilities: each method verifies a single data type.
+
+The class has no data members and only static methods because the purpose of the class is to package similar
+methods.
--- Tests/InputControl/test_InputController.py
+++ Tests/InputControl/test_InputController.py
@@ -1,19 +1,44 @@
 from RealEstateValuationSystem.InputControl.InputController import InputController
+import pandas
 
 class TestInputController(object):
-	def test_IsDict(self):
+	def test_IsDictT(self):
 		dict = {'A' : 1}
 		assert InputController.IsDict(dict) == True
 		
-	def test_IsBool(self):
+	def test_IsDictF(self):
+		list = []
+		assert InputController.IsDict(dict) == False
+		
+	def test_IsBoolT(self):
 		boolean = True
 		assert InputController.IsBool(boolean) == True
 		
-	def test_IsList(self):
+	def test_IsBoolF(self):
+		dict = {}
+		assert InputController.IsBool(dict) == False
+		
+	def test_IsListT(self):
 		list = []
 		assert InputController.IsList(list) == True
 		
-	def test_IsDataFrame(self):
+	def test_IsListF(self):
+		boolean = False
+		assert InputController.IsList(boolean) == False
+		
+	def test_IsDataFrameT(self):
+		frame = pandas.DataFrame()
+		assert InputController.IsDataFrame(frame) == True
+		
+	def test_IsDataFrameF(self):
 		list = []
 		assert InputController.IsDataFrame(list) == False
+		
+	def test_IsStringT(self):
+		string = 'string'
+		assert InputController.IsString(string) == True
+		
+	def test_IsStringF(self):
+		list = []
+		assert InputController.IsString(list) == False
 		
